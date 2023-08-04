b1adb4c5facd0d36d5297b5a4efbc1db24f97c09
Robert Brown <rjbrown124@comcast.net>
2023-08-03 20:08:16 -0400

    Fixing Import statements and references

--- src/main/java/frc/robot/TankModule.java
+++ src/main/java/frc/robot/TankModule.java
@@ -1,47 +1,65 @@
 package frc.robot;
 
-public class TankModule {
-	
-	private TalonSRX /**FX*/ frontMotor;
-	private TalonSRX /**FX*/ rearMotor;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj.Encoder;
+
+public class TankModule
+	{
+
+	private WPI_TalonFX /** FX */
+	frontMotor;
+	private WPI_TalonFX /** FX */
+	rearMotor;
 	private Encoder frontEncoder;
 	private Encoder rearEncoder;
-	
-	public TankModule(int frontID, int rearID, int feID, int reID) {
-		
-		frontMotor = new TalonSRX(frontID);
-		rearMotor = new TalonSRX(rearID);
-		
-		frontEncoder = new Encoder(feID);
-		rearEncoder = new Encoder(reID);
-		
-	}
-	
-	//Double check 624 implementation of desiredState, they dont explicity pass in control here
-	public boolean setDesiredState(/**TeleopModuleState desiredState,*/ double speed) {
-		//for swerve call setAngle First!!!!!!1 - highly important
-		if(setSpeed(/**desiredState,*/speed)) {
-			return true;
+
+	public TankModule(int frontID, int rearID, int feID, int reID)
+		{
+
+			frontMotor = new WPI_TalonFX(frontID);
+			rearMotor = new WPI_TalonFX(rearID);
+
+			// frontEncoder = frontMotor.
+
 		}
+
+	// Double check 624 implementation of desiredState, they dont explicity pass
+	// in control here
+	public boolean setDesiredState(/** TeleopModuleState desiredState, */
+	double speed)
+	{
+		// for swerve call setAngle First!!!!!!1 - highly important
+		if (setSpeed(/** desiredState, */
+				speed))
+			{
+			return true;
+			}
+		return false;
 	}
-	
-	private boolean setSpeed(/**TeleopModuleState desiredState,*/ double speed) {
-		//this representation is redundant but it due to a difference in teleop/swerve
-		if(true) {
-			//use correct Talon / other set method here
+
+	private boolean setSpeed(/** TeleopModuleState desiredState, */
+	double speed)
+	{
+		// this representation is redundant but it due to a difference in
+		// teleop/swerve
+		if (true)
+			{
+			// use correct Talon / other set method here
 			frontMotor.set(speed);
 			rearMotor.set(speed);
-		}else {
-			//swerve calculation
-		}
+			}
+		else
+			{
+			// swerve calculation
+			}
 		return true;
 	}
-	
-	//In Swerve this will return an object instead of a raw value
-	public double getEncoderValue() {
-		//fluff math - not true implementation
-		return (frontEncoder + rearEncoder) / 2; 
-	}
-	
 
-}
+	// In Swerve this will return an object instead of a raw value
+	// public double getEncoderValue()
+	// {
+	// // fluff math - not true implementation
+	// return (frontEncoder + rearEncoder) / 2;
+	// }
+
+	}
--- src/main/java/frc/robot/subsystems/TankTransmission.java
+++ src/main/java/frc/robot/subsystems/TankTransmission.java
@@ -6,41 +6,43 @@ import frc.robot.Constants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.sensors.Pigeon2;
 
-
-
-public class TankTransmission extends SubsystemBase{
+public class TankTransmission extends SubsystemBase
+	{
 	public TankModule[] tankTreads;
 	public Pigeon2 gyro;
-	
-	
-	public Swerve() {
-		gyro = new Pigeon2(Constants.gyroID);
-		
-		tankTreads = new TankModule[] {
-				new TankModule(Constants.leftFrontID, Constants.leftRearID, Constants.leftFrontEncoder, Constants.leftRearEncoder),
-				new TankModule(Constants.rightFrontID, Constants.rightRearID, Constants.rightFrontEncoder, Constants.rightRearEncoder)			
-		};
-		
-	}
-	
-	public void drive(double speed, double otherSpeed) {
-		
-		//Swerve Implementation will look something like this with some odometry math above to calculate what values to pass
-//		for(TankModule tread : tankTreads) {
-//			tread.setDesiredState(speed);
-//		}
-		
-		//for tank transmission we can simply do this for now
+
+	public TankTransmission()
+		{
+			// gyro = new Pigeon2(Constants.gyroID);
+
+			// tankTreads = new TankModule[] {
+			// new TankModule(Constants.leftFrontID, Constants.leftRearID,
+			// Constants.leftFrontEncoder, Constants.leftRearEncoder),
+			// new TankModule(Constants.rightFrontID, Constants.rightRearID,
+			// Constants.rightFrontEncoder, Constants.rightRearEncoder)
+			// };
+
+		}
+
+	public void drive(double speed, double otherSpeed)
+	{
+
+		// Swerve Implementation will look something like this with some
+		// odometry math above to calculate what values to pass
+		// for(TankModule tread : tankTreads) {
+		// tread.setDesiredState(speed);
+		// }
+
+		// for tank transmission we can simply do this for now
 		tankTreads[0].setDesiredState(speed);
 		tankTreads[1].setDesiredState(otherSpeed);
 	}
-	
-	
+
 	@Override
-	public void periodic() {
-		//This block of code runs once per schedular 'tick'
-		//Pretty much do nothing in this section
+	public void periodic()
+	{
+		// This block of code runs once per schedular 'tick'
+		// Pretty much do nothing in this section
 	}
-	
-	
-}
+
+	}
