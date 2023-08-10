54105564f7c282b054aea2d3629dcade674b30cc
Robert Brown <rjbrown124@comcast.net>
2023-08-10 19:01:41 -0400

    Rename to match new standard

--- src/main/java/frc/robot/commands/TeleopDrive.java
+++ src/main/java/frc/robot/commands/TeleopDrive.java
@@ -1,29 +1,32 @@
 package frc.robot.commands;
 
-
 import frc.robot.Constants;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import java.util.function.DoubleSupplier;
 
-import frc.robot.subsystems.TankTransmission;
+import frc.robot.subsystems.TankSubsystem;
 
-
-public class TeleopDrive extends CommandBase  {
-	private TankTransmission tank;
-	DoubleSupplier leftDrive; 
+public class TeleopDrive extends CommandBase
+	{
+	private TankSubsystem tank;
+	DoubleSupplier leftDrive;
 	DoubleSupplier rightDrive;
-	
-	public TeleopDrive(TankTransmission tank, DoubleSupplier leftDrive, DoubleSupplier rightDrive) {
-		this.tank = tank;
-		this.leftDrive = leftDrive;
-		this.rightDrive = rightDrive;
-	}
-	
-	//This is the only method that is required
-	//execute determines the functionality of the command - What will this command call
+
+	public TeleopDrive(TankSubsystem tank, DoubleSupplier leftDrive, Do
+			bleSupplier rightDrive) {
+		
+			this.tank = tank;
+			this.leftDrive = leftDrive;
+			this.rightDrive = rightDrive;
+		}
+
+	// This is the only method that is required
+	// execute determines the functionality of the command - What will this
+	// command call
 	@Override
-	public void execute() {
+	public void execute()
+	{
 		tank.drive(leftDrive.getAsDouble(), rightDrive.getAsDouble());
 	}
-	
-}
+
+	}
