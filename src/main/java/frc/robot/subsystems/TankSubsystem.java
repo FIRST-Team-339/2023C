fe3cccc327ddb82c37213ef8740de4495854a260
fisherjacobc <fisherjc2008@gmail.com>
2023-08-30 12:32:52 -0400

    chore: import some hardware interfaces

--- src/main/java/frc/robot/subsystems/TankSubsystem.java
+++ src/main/java/frc/robot/subsystems/TankSubsystem.java
@@ -6,98 +6,248 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.*;
+import frc.robot.enums.DriveGears;
 import frc.robot.hardwareInterfaces.KilroyEncoder;
 
 public class TankSubsystem extends SubsystemBase
 	{
-	// Motors on the Left Side
+	/* Motors on the Left Side */
 	private MotorController motorForLeftEncoder = new WPI_TalonFX(
 			DriveConstants.BOTTOM_LEFT_MOTOR_ID);
 	private MotorControllerGroup leftMotorControllerGroup = new MotorControllerGroup(
 			new WPI_TalonFX(DriveConstants.TOP_LEFT_MOTOR_ID),
 			motorForLeftEncoder);
 
-	// Motors on the Right Side
+	/* Motors on the Right Side */
 	private MotorController motorForRightEncoder = new WPI_TalonFX(
 			DriveConstants.BOTTOM_RIGHT_MOTOR_ID);
 	private MotorControllerGroup rightMotorControllerGroup = new MotorControllerGroup(
 			new WPI_TalonFX(DriveConstants.TOP_RIGHT_MOTOR_ID),
 			motorForRightEncoder);
 
-	// The Robot's Drive
+	/* The Robot's Drive */
 	private DifferentialDrive differentialDrive = new DifferentialDrive(
 			leftMotorControllerGroup, rightMotorControllerGroup);
 
-	// The Drive Encoder on the Left Side
+	/* The Drive Encoder on the Left Side */
 	private KilroyEncoder leftEncoder = new KilroyEncoder(
 			(WPI_TalonFX) motorForLeftEncoder);
 
-	// The Drive Encoder on the Left Side
+	/* The Drive Encoder on the Left Side */
 	private KilroyEncoder rightEncoder = new KilroyEncoder(
 			(WPI_TalonFX) motorForRightEncoder);
 
-	// Current Gear
-	private DriveConstants.DRIVE_GEARS currentGear;
+	/* Current Gear */
+	private DriveGears currentGear;
 
 	public TankSubsystem()
 		{
-			// Set the `currentGear` value to the passed `startingGear` value
+			/* Set the currentGear value to the passed startingGear value */
 			currentGear = DriveConstants.defaultGear;
 			setGear(currentGear);
 
-			// Set the Joystick Deadband
+			/* Set the Joystick Deadband */
 			differentialDrive.setDeadband(DriveConstants.JOYSTICK_DEADBAND);
 
-			// Set the inversion value for the motor controller groups
+			/* Set the inversion value for the motor controller groups */
 			leftMotorControllerGroup.setInverted(
 					DriveConstants.MOTOR_CONTROLLER_GROUPS_INVERTED[0]);
 			rightMotorControllerGroup.setInverted(
 					DriveConstants.MOTOR_CONTROLLER_GROUPS_INVERTED[1]);
 
-			// Reset Encoders
+			/* Reset Encoders */
 			leftEncoder.reset();
 			rightEncoder.reset();
 		}
 
-	public void drive(double leftSpeed, double rightSpeed)
+	/**
+	 * Get the Left Encoder
+	 * 
+	 * @return The Left Encoder
+	 */
+	public KilroyEncoder getLeftEncoder()
+	{
+		return leftEncoder;
+	}
+
+	/**
+	 * Get the Right Encoder
+	 * 
+	 * @return The Right Encoder
+	 */
+	public KilroyEncoder getRightEncoder()
+	{
+		return rightEncoder;
+	}
+
+	/**
+	 * Checks if the {@code encoder} has passed the {@code distance} provided
+	 * 
+	 * @param distance
+	 *            The distance to check
+	 * @return If any of the {@code encoder} has passed the {@code distance}
+	 *         provided
+	 */
+	public boolean encoderHasPassedDistance(KilroyEncoder encoder,
+			double distance)
+	{
+		return encoder.getDistance() >= distance;
+	}
+
+	/**
+	 * Checks if either encoders (left or right) have passed the
+	 * {@code distance} provided
+	 * 
+	 * @param distance
+	 *            The distance to check
+	 * @return If any of the encoders (left or right) have passed the
+	 *         {@code distance} provided
+	 */
+	public boolean anyEncoderHasPassedDistance(double distance)
+	{
+		return encoderHasPassedDistance(leftEncoder, distance)
+				|| encoderHasPassedDistance(rightEncoder, distance);
+	}
+
+	/**
+	 * Drives the robot straight with the {@link DifferentialDrive#tankDrive}
+	 * method
+	 * 
+	 * @param leftSpeed
+	 *            The robot's left side speed along the X axis [-1.0..1.0].
+	 *            Forward is positive.
+	 * @param rightSpeed
+	 *            The robot's right side speed along the X axis [-1.0..1.0].
+	 *            Forward is positive.
+	 */
+	public void driveStraight(double leftSpeed, double rightSpeed)
 	{
 		differentialDrive.tankDrive(leftSpeed, rightSpeed);
 	}
 
-	public DriveConstants.DRIVE_GEARS getCurrentGear()
+	/**
+	 * Drives the robot straight a certain number of inches
+	 * 
+	 * @param distance
+	 *            The distance you want the robot to travel
+	 * @param speed
+	 *            The robot's speed along the X axis [-1.0..1.0]. Forward is
+	 *            positive.
+	 * @param resetEncoders
+	 *            If you want to reset the encoders (usually this would be done
+	 *            once per Command)
+	 * @return
+	 */
+	public boolean driveStraightInches(double distance, double speed,
+			boolean resetEncoders)
+	{
+		if (resetEncoders == true)
+			{
+			leftEncoder.reset();
+			rightEncoder.reset();
+			}
+
+		if (anyEncoderHasPassedDistance(distance) == true)
+			{
+			return true;
+			}
+		else
+			{
+			driveStraight(speed, speed);
+			return false;
+			}
+	}
+
+	/**
+	 * Get the current gear of the {@link TankSubsystem}
+	 * 
+	 * @return A {@link DriveGears} enum
+	 */
+	public DriveGears getCurrentGear()
 	{
 		return currentGear;
 	}
 
+	/**
+	 * Get the current gear ratio (max output) of the {@link TankSubsystem}
+	 * 
+	 * @return The current gear ratio
+	 */
 	public double getCurrentGearRatio()
 	{
 		return currentGear.getRatio();
 	}
 
-	public void setGear(DriveConstants.DRIVE_GEARS gear)
+	/**
+	 * Set the gear (the {@link DifferentialDrive}'s max output)
+	 * 
+	 * @param gear
+	 *            The desired gear to set the max output as
+	 */
+	public void setGear(DriveGears gear)
 	{
 		differentialDrive.setMaxOutput(gear.getRatio());
 	}
 
-	private void shiftGearBy(int shiftBy)
+	/**
+	 * Shift the gear up or down
+	 * 
+	 * @param shiftBy
+	 *            How much you want to shift by (example: +1)
+	 * @return If the gear successfully shifted (will return false if you
+	 *         shifted to a gear that doesn't exist)
+	 */
+	public boolean shiftGearBy(int shiftBy)
 	{
-		DriveConstants.DRIVE_GEARS newGear = DriveConstants.DRIVE_GEARS
+		DriveGears newGear = DriveGears
 				.getFromId(currentGear.getId() + shiftBy);
 		if (newGear != null)
 			{
 			setGear(newGear);
 			currentGear = newGear;
+			return true;
 			}
+		return false;
 	}
 
-	public void shiftGearUp()
+	/**
+	 * Shift the gear up by 1
+	 * 
+	 * @return If the gear successfully shifted (will return false if you
+	 *         shifted to a gear that doesn't exist)
+	 */
+	public boolean shiftGearUp()
 	{
-		shiftGearBy(1);
+		return shiftGearBy(1);
 	}
 
-	public void shiftGearDown()
+	/**
+	 * Shift the gear down by 1
+	 * 
+	 * @return If the gear successfully shifted (will return false if you
+	 *         shifted to a gear that doesn't exist)
+	 */
+	public boolean shiftGearDown()
 	{
-		shiftGearBy(-1);
+		return shiftGearBy(-1);
+	}
+
+	/**
+	 * Shift to the lowest (first) gear
+	 */
+	public void shiftLowestGear()
+	{
+		DriveGears lowestGear = DriveGears.getFromId(0);
+		setGear(lowestGear);
+	}
+
+	/**
+	 * Shift to the highest gear
+	 */
+	public void shiftHighestGear()
+	{
+		DriveGears highestGear = DriveGears.getFromId(-1);
+		setGear(highestGear);
 	}
 
 	}
