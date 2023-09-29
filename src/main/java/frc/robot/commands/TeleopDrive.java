373770a614769a395a643c2bf2efbc68b2da1d13
fisherjacobc <fisherjc2008@gmail.com>
2023-09-29 09:56:32 -0400

    feat(TeleopDrive): smooth acceleration to prevent jolting

--- src/main/java/frc/robot/commands/TeleopDrive.java
+++ src/main/java/frc/robot/commands/TeleopDrive.java
@@ -1,13 +1,13 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import java.util.function.DoubleSupplier;
 import frc.robot.Constants.*;
 import frc.robot.subsystems.TankSubsystem;
 import frc.robot.subsystems.DashboardSubsystem;
 
-public class TeleopDrive extends CommandBase
-	{
+public class TeleopDrive extends CommandBase {
 	/* Subsystems */
 	private TankSubsystem tankSubsystem;
 	private DashboardSubsystem dashboardSubsystem;
@@ -15,6 +15,10 @@ public class TeleopDrive extends CommandBase
 	/* Left + Right Drive */
 	private DoubleSupplier leftDrive;
 	private DoubleSupplier rightDrive;
+
+	/* Limiter */
+	SlewRateLimiter accelerationLimiter = new SlewRateLimiter(DriveConstants.ACCELERATION_RATE_LIMIT);
+	public boolean accelerationLimiterReset = true;
 
 	/**
 	 * Constructor
@@ -24,28 +28,28 @@ public class TeleopDrive extends CommandBase
 	 */
 	public TeleopDrive(TankSubsystem tankSubsystem,
 			DashboardSubsystem dashboardSubsystem, DoubleSupplier leftDrive,
-			DoubleSupplier rightDrive)
-		{
-			addRequirements(tankSubsystem, dashboardSubsystem);
-			this.tankSubsystem = tankSubsystem;
-			this.dashboardSubsystem = dashboardSubsystem;
+			DoubleSupplier rightDrive) {
+		addRequirements(tankSubsystem, dashboardSubsystem);
+		this.tankSubsystem = tankSubsystem;
+		this.dashboardSubsystem = dashboardSubsystem;
 
-			this.leftDrive = leftDrive;
-			this.rightDrive = rightDrive;
-		}
+		this.leftDrive = leftDrive;
+		this.rightDrive = rightDrive;
+	}
 
 	@Override
-	public void initialize()
-	{
+	public void initialize() {
 		dashboardSubsystem
 				.updateAutoIndicator(DashboardSubsystem.AutonomousMode.Teleop);
 	}
 
 	@Override
-	public void execute()
-	{
-		tankSubsystem.driveStraight(leftDrive.getAsDouble(),
-				rightDrive.getAsDouble());
+	public void execute() {
+		double smoothLeftSpeed = accelerationLimiter.calculate(-leftDrive.getAsDouble());
+		double smoothRightSpeed = accelerationLimiter.calculate(-rightDrive.getAsDouble());
+
+		tankSubsystem.drive(smoothLeftSpeed,
+				smoothRightSpeed);
 
 		boolean inReverse = leftDrive
 				.getAsDouble() > DriveConstants.JOYSTICK_DEADBAND
@@ -55,4 +59,4 @@ public class TeleopDrive extends CommandBase
 				inReverse);
 	}
 
-	}
+}
